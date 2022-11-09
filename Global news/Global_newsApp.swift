4fd5efc9d54ef965b215d617f542b55e5cabc41e
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-09 23:20:35 +0200

    Initial Commit

---
+++ Global news/Global_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Global_newsApp.swift
+//  Global news
+//
+//  Created by Hugo on 9.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Global_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
