031eb4c219106286d26695e810b328e7af3cddb2
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-11 00:23:12 +0200

    news model

---
+++ Global news/Models/News.swift
@@ -0,0 +1,56 @@
+//
+//  News.swift
+//  Global news
+//
+//  Created by Hugo on 10.11.22.
+//
+
+import Foundation
+
+struct News: Identifiable {
+    let id = UUID()
+    let image: String
+    let title: String
+    let category: String
+    let author: String
+    let content: String
+    let uploadTime: String
+    let numberOfComments: Int
+}
+
+struct NewsList {
+    static let allNews = [
+        // MARK: 5 tourist attractions that you must visit while in Indonesia
+        News(image: "5-tourist-attractions-in",
+             title: "5 tourist attractions that you must visit while in Indonesia",
+             category: "Travel",
+             author: "Achmad Alimin",
+             content: "Lorem ispum for now",
+             uploadTime: "15:34",
+             numberOfComments: 23),
+        // MARK: Take your friends to a new hangout in Jakarta, here's the list!
+        News(image: "friends-hangout",
+             title: "Take your friends to a new hangout in Jakarta, here's the list!",
+             category: "Travel",
+             author: "Ikka Damayana",
+             content: "Lorem ispum for now",
+             uploadTime: "12:34",
+             numberOfComments: 12),
+        // MARK: Alert! The following characteristics of Child victims of bullying
+        News(image: "child-bullying",
+             title: "Alert! The following characteristics of Child victims of bullying",
+             category: "Education",
+             author: "Ikka Damayan",
+             content: "Lorem ispum for now",
+             uploadTime: "9:00",
+             numberOfComments: 6),
+        // MARK: Facebook and Instagram owner Meta to lay off more than 11,000 staff
+        News(image: "meta-lay-off",
+             title: "Facebook and Instagram owner Meta to lay off more than 11,000 staff",
+             category: "Technology",
+             author: "James Sillars",
+             content: "Lorem ispum for now",
+             uploadTime: "5:45",
+             numberOfComments: 120)
+    ]
+}
