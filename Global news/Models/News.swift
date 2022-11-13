1a2040fec0512040a781ce4c695bdcfa9fad275a
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-13 12:34:47 +0200

    bug fix

--- Global news/Models/News.swift
+++ Global news/Models/News.swift
@@ -25,7 +25,7 @@ struct NewsList {
              title: "Take your friends to a new hangout in Jakarta, here's the list!",
              category: "Travel",
              author: "Ikka Damayana",
-             content: "Lorem ispum for now",
+             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
              uploadTime: "12:34",
              numberOfComments: 12),
         // MARK: Alert! The following characteristics of Child victims of bullying
@@ -33,7 +33,7 @@ struct NewsList {
              title: "Alert! The following characteristics of Child victims of bullying",
              category: "Education",
              author: "Ikka Damayan",
-             content: "Lorem ispum for now",
+             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
              uploadTime: "9:00",
              numberOfComments: 6),
         // MARK: Facebook and Instagram owner Meta to lay off more than 11,000 staff
@@ -41,7 +41,7 @@ struct NewsList {
              title: "Facebook and Instagram owner Meta to lay off more than 11,000 staff",
              category: "Technology",
              author: "James Sillars",
-             content: "Lorem ispum for now",
+             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
              uploadTime: "5:45",
              numberOfComments: 120)
     ]
--- Global news/Models/TrendingNews.swift
+++ Global news/Models/TrendingNews.swift
@@ -33,7 +33,7 @@ struct TrendingCarousel {
                      title: "FTX: Crypto giant collapses into bankruptcy",
                      category: "Technology",
                      author: "Natalie Sherman",
-                     content: "Embattled cryptocurrency exchange FTX has filed for bankruptcy in the US, seeking court protection as it looks for a way to return money to users. Former boss Sam Bankman-Fried has also stepped down as chief executive, the company said. It is a massive turn of fortunes for the 30-year-old, who had headed the world's second largest crypto exchange. In just over a week, his FTX empire has collapsed, shaking confidence in the already troubled crypto market. \"I'm really sorry, again, that we ended up here. Hopefully things can find a way to recover,\" Mr Bankman-Fried, nicknamed the 'King of Crypto', wrote on Twitter on Friday. \"I was shocked to see things unravel the way they did.\"",
+                     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                      uploadTime: "20:17",
                      numberOfComments: 65),
         // MARK: Cassius Turvey: Mother's grief at killing that's shocked Australia
